cf92b15b9a1c2dc4f0f355f1807b62154464d4a6
quyen0505 <quyen280505@gmail.com>
2025-06-06 00:18:17 +0700

    RoomDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import RoomDetails from './pages/RoomDetails'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      {!isOwnerpath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/rooms/:id' element={<RoomDetails/>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
