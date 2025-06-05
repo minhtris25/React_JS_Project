85668601d1df4d719f37c17bf1d42ae3bc04c4e7
minhtris25 <tomleeq258@gmail.com>
2025-06-06 01:02:17 +0700

    Remove tracked node_modules and .env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import RoomDetails from './pages/RoomDetails'
 import { useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes('owner');
@@ -8,6 +10,11 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+              <Routes>
+                <Route path='/rooms/:id' element={<RoomDetails/>} />
+              </Routes>
+            </div>
     </div>
   );
 };
