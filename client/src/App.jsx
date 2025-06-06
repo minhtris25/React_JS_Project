2ca622a740bc13224622f614215493d902b72b2d
quyen0505 <quyen280505@gmail.com>
2025-06-06 09:27:42 +0700

    MyBookings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import RoomDetails from './pages/RoomDetails'
 import { useLocation } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
+import MyBookings from './pages/MyBookings';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes('owner');
@@ -13,6 +14,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
               <Routes>
                 <Route path='/rooms/:id' element={<RoomDetails/>} />
+                <Route path='/my-bookings' element={<MyBookings/>} />
               </Routes>
             </div>
     </div>
