6e7b0a70e5d7468042ef6f969bd7d07bd697d39b
minhtris25 <tomleeq258@gmail.com>
2025-06-11 09:18:27 +0700

    connet

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   port: 587,
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    pass: "xsmtpsib-450b7d18f4addfa8aa0cd8ed140b44a2bdd172d47fbb7e260d7a8b9ac17ac3a8-L7rspVchwdGCNRaJ",
   },
 })
 
