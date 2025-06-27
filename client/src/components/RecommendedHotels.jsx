e0fb2d0c95e7f9272ab2b93c27dfd813c42c9c37
minhtris25 <tomleeq258@gmail.com>
2025-06-27 15:40:07 +0700

    viet hoa

--- client/src/components/RecommendedHotels.jsx
+++ client/src/components/RecommendedHotels.jsx
@@ -1,41 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HotelCard from './HotelCard';
 import Title from './Title';
-import { useNavigate } from 'react-router-dom';
 import { useAppContext } from '../conext/AppContext';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 const RecommendedHotels = () => {
-    const {rooms,searchedCities} = useAppContext();
+    const { rooms, searchedCities } = useAppContext();
     const [recommended, setRecommended] = useState([]);
 
-    const filterHotels = ()=>{
-        const filterHotels = rooms.slice().filter( room => searchedCities.includes(room.hotel.city));
-        setRecommended(filterHotels);
+    useEffect(() => {
+        if (searchedCities.length > 0) {
+            const filteredHotels = rooms.filter(room => searchedCities.includes(room.hotel.city));
+            setRecommended(filteredHotels);
+        } else {
+            setRecommended([]);
+        }
+    }, [rooms, searchedCities]);
 
-    }
+    if (recommended.length === 0) return null;
 
-    useEffect(()=>{
-        filterHotels()
-    },[rooms, searchedCities])
-    
-  return recommended.length >0 && (
-    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
-
-        <Title title='Khách Sạn Được Đề Xuất' subTitle='Khám phá bộ sưu tập được chúng tôi tuyển chọn kỹ lưỡng về các bất động sản đặc biệt
-trên khắp thế giới, mang đến sự sang trọng vô song và những trải nghiệm khó quên'>
-
-        </Title>
-
-      <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-        {recommended.slice(0, 4).map((room, index) => (
-          <HotelCard key={room._id} room={room} index={index} />
-        ))}
-      </div>
-     
-    </div>
-  );
+    return (
+        <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+            <Title
+                title='Khách Sạn Được Đề Xuất'
+                subTitle='Khám phá bộ sưu tập được chúng tôi tuyển chọn kỹ lưỡng về các bất động sản đặc biệt trên khắp thế giới, mang đến sự sang trọng vô song và những trải nghiệm khó quên'
+            />
+            <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
+                {recommended.slice(0, 4).map((room, index) => (
+                    <HotelCard key={room._id} room={room} index={index} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default RecommendedHotels;
